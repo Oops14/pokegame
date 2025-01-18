a23a8d05d1332cdb56ddef137983b9cb90641f5b
Nazar <ups14q@gmail.com>
2025-01-18 12:12:46 +0200

    accordion

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -8,6 +8,7 @@ import arrow_top from '@/assets/images/arrows/arrow_top.svg'
 import PokemonItem from '@/components/pokemonItem/PokemonItem.tsx'
 
 import { Pokemon } from '@/utils/types/apiTypes/apiTypes.ts'
+import { ACCORDION } from '@/utils/enums/accordionEnum/accordionEnum'
 
 import Typography from '@/ui/typography/Typography'
 
@@ -21,6 +22,27 @@ interface AccordionProps {
 
 export const Accordion: FC<AccordionProps> = ({ children, id, pokemons }) => {
   const [isOpen, setIsOpen] = useState(false)
+
+  const rows = 7
+  const cols = 7
+
+  const [grid, setGrid] = useState(
+    Array(rows)
+      .fill(null)
+      .map(() => Array(cols).fill(false))
+  )
+
+  console.log(grid)
+
+  // Handle growing barberries in a cell
+  const growBarberry = (row: number, col: number) => {
+    // Clone grid state
+    const newGrid = grid.map((r) => [...r])
+
+    // Mark the cell as containing a barberry
+    newGrid[row][col] = true
+    setGrid(newGrid)
+  }
 
   const handleOpen = () => {
     setIsOpen(!isOpen)
@@ -36,10 +58,27 @@ export const Accordion: FC<AccordionProps> = ({ children, id, pokemons }) => {
       </div>
       <div className={clsx(s.accordion__bottom_content, { [s.opened]: isOpen })}>
         <div className={clsx(s.grid_pokemons, { [s.show_grid]: isOpen })}>
-          {id === 1 &&
+          {id === ACCORDION.MY_POKEMONS &&
             pokemons.map((i, indx) => {
               return <PokemonItem key={indx} title={i.name} />
             })}
+
+          {id === ACCORDION.GARDEN && (
+            <div className={s.grid_main_garden}>
+              <div className={s.grid_garden}>
+                {grid.map((row, rowIndex) =>
+                  row.map((cell, colIndex) => (
+                    <div
+                      key={`${rowIndex}-${colIndex}`}
+                      className={`cell ${cell ? 'barberry' : ''}`}
+                      onClick={() => growBarberry(rowIndex, colIndex)}>
+                      {cell && <div className="barberry-icon"></div>}
+                    </div>
+                  ))
+                )}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
---
+++ src/utils/enums/accordionEnum/accordionEnum.ts
@@ -0,0 +1,5 @@
+export enum ACCORDION {
+  MY_POKEMONS = 1,
+  GARDEN = 2,
+  HUNT = 3,
+}
